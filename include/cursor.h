f3b1c0b7f9452271738724efe9594707d61ffeab
Gabriel <gbrlferz@gmail.com>
2025-01-03 20:13:04 -0300

    cursor.h refactor

--- include/cursor.h
+++ include/cursor.h
@@ -3,11 +3,8 @@
 #include "../include/raylib.h"
 
 struct Cursor {
- public:
   Vector2 position;
   Vector2 size;
   // Constructor
   Cursor(Vector2 pos, Vector2 sz) : position(pos), size(sz) {}
 };
-
-void MoveCursor(Cursor cursor, std::vector<std::string> tileMap);
